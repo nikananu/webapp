c6819560beec70d082dbde0b59f8ec99fccfee6b
nikananu <n_nanuashvili@cu.edu.ge>
2025-02-05 14:10:34 +0400

    Add project files.

---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            var per1 = new test
+            {
+                name = "Nika "
+            };
+            return View(per1);
+        }
+        public IActionResult ConfirmationPage()
+        {
+            return View();
+        }
+        public IActionResult NoConfirmationPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/test.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class test
+    {
+        public string name {  get; set; }
+
+    }
+}
